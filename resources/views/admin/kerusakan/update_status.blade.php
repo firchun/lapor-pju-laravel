56bbc69d4e17c201940da1bba8875fcb2e51b227
firchun025 <firchun025@gmail.com>
2024-07-13 18:06:34 +0900

    back camera scann

--- resources/views/admin/kerusakan/update_status.blade.php
+++ resources/views/admin/kerusakan/update_status.blade.php
@@ -6,9 +6,18 @@
         <div class="col-md-8">
             <div class="card-box mb-30">
                 <div class="card-body">
-                    <b class="text-danger text-center">*Harap arahkan pada QRcode PJU</b>
-                    <div id="scanner-container">
+                    {{-- <div id="scanner-container">
                         <video id="scanner" width="100%"></video>
+                    </div> --}}
+                    <div class="form-group text-center">
+                        <b class="text-danger text-center">*Harap arahkan pada QRcode PJU</b>
+                        <div id="loadingMessage">Tidak dapat mengakses kamera (Mohon untuk mengaktifkan
+                            pengaturan kamera)</div>
+                        <canvas id="canvas" hidden style="width: 80% !important;"></canvas>
+                        <div id="output" hidden>
+                            <div id="outputMessage">Qr code tidak terdeteksi, harap perbaiki posisi kamera</div>
+                            <div hidden><b>Data:</b> <span id="outputData"></span></div>
+                        </div>
                     </div>
                     <form id="qr-form" action="{{ route('kerusakan.update') }}" method="GET">
                         <div class="mb-4" style="display: none;"> <!-- Hidden input code -->
@@ -17,18 +26,19 @@
                                 required>
                         </div>
                         <button type="submit" id="submit-btn" class="btn btn-primary btn-block"
-                            style="display: none;">Update</button> <!-- Hidden submit button -->
+                            style="display: none;">Update</button>
+                        <!-- Hidden submit button -->
                     </form>
-                    <button id="scan-btn" class="btn btn-primary btn-block" style="display: none;">Scan QR Code</button>
                 </div>
             </div>
         </div>
     </div>
 
     <!-- Include Instascan Library -->
-    <script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
-
-    <script>
+    {{-- <script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script> --}}
+    <script src="{{ asset('/') }}js/app-qr.js" type="text/javascript"></script>
+    <script src="{{ asset('/') }}js/jsQR.js" type="text/javascript"></script>
+    {{-- <script>
         // Function to initialize QR scanner
         function initScanner() {
             let scanner = new Instascan.Scanner({
@@ -46,22 +56,14 @@
                 document.getElementById('qr-form').submit();
             });
 
-            // Function to find the back camera
-            function findBackCamera(cameras) {
-                for (let i = 0; i < cameras.length; i++) {
-                    if (cameras[i].name.toLowerCase().includes('back')) {
-                        return cameras[i];
-                    }
-                }
-                return cameras[0]; // Return the first camera if no back camera is found
-            }
-
-            // Get cameras and start scanning
             Instascan.Camera.getCameras().then(function(cameras) {
-                let backCamera = findBackCamera(cameras);
-                scanner.start(backCamera);
+                let selectedCamera = cameras.find(camera => camera.name.includes('back'));
+                if (!selectedCamera) {
+                    selectedCamera = cameras[0]; // Pilih kamera pertama jika tidak ada kamera belakang
+                }
+                scanner.start(selectedCamera);
             }).catch(function(e) {
-                console.error('Error initializing scanner:', e);
+                console.error(e);
             });
 
             // Menampilkan tombol scan dan menyembunyikan form dan tombol submit
@@ -76,5 +78,78 @@
         document.addEventListener('DOMContentLoaded', function() {
             initScanner();
         });
+    </script> --}}
+    <script>
+        function refreshPage() {
+            window.location.reload();
+        }
+
+        var video = document.createElement("video");
+        var canvasElement = document.getElementById("canvas");
+        var canvas = canvasElement.getContext("2d");
+        var loadingMessage = document.getElementById("loadingMessage");
+        var outputContainer = document.getElementById("output");
+        var outputMessage = document.getElementById("outputMessage");
+        var outputData = document.getElementById("outputData");
+        var submit = document.getElementById("submit-btn");
+        var outputDatas = document.getElementById("code");
+
+        function drawLine(begin, end, color) {
+            canvas.beginPath();
+            canvas.moveTo(begin.x, begin.y);
+            canvas.lineTo(end.x, end.y);
+            canvas.lineWidth = 4;
+            canvas.strokeStyle = color;
+            canvas.stroke();
+        }
+
+        // Use facingMode: environment to attemt to get the front camera on phones
+        navigator.mediaDevices.getUserMedia({
+            video: {
+                facingMode: "environment"
+            }
+        }).then(function(stream) {
+            video.srcObject = stream;
+            video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+            video.play();
+            requestAnimationFrame(tick);
+        });
+
+        function tick() {
+            loadingMessage.innerText = "Loading camera..."
+            if (video.readyState === video.HAVE_ENOUGH_DATA) {
+                loadingMessage.hidden = true;
+                canvasElement.hidden = false;
+                outputContainer.hidden = false;
+
+                // canvasElement.height = video.videoHeight;
+                // canvasElement.width = video.videoWidth;
+                // Adjust canvas size to match video frame
+                canvasElement.height = 400;
+                canvasElement.width = 400;
+                canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+                var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+                var code = jsQR(imageData.data, imageData.width, imageData.height, {
+                    inversionAttempts: "dontInvert",
+                });
+                if (code) {
+                    drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF3B58");
+                    drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF3B58");
+                    drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF3B58");
+                    drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF3B58");
+                    outputMessage.hidden = true;
+                    outputData.parentElement.hidden = true;
+                    outputData.innerText = code.data.substr(-16);
+                    outputDatas.value = code.data.substr(-16);
+
+                    // Submit form 'qr-form'
+                    document.getElementById('qr-form').submit();
+                } else {
+                    outputMessage.hidden = false;
+                    outputData.parentElement.hidden = true;
+                }
+            }
+            requestAnimationFrame(tick);
+        }
     </script>
 @endsection
