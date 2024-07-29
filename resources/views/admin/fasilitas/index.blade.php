2dbfc6abc1610139537255f8eaf4e480667a32bd
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2024-07-29 23:12:19 +0900

    test

--- resources/views/admin/fasilitas/index.blade.php
+++ resources/views/admin/fasilitas/index.blade.php
@@ -18,7 +18,7 @@
                 </span>
             </button>
         </div>
-    </div>
+    </div>  
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card-box mb-30">
